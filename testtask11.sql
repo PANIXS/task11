086b5ae573191e72260f08c592a73ef67c774541
Li Pan <1160728236@qq.com>
2018-03-05 16:25:06 +0800

    r

--- testtask11.sql
+++ testtask11.sql
@@ -1,21 +1,41 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : mysql
+ Source Server         : bookbest
  Source Server Type    : MySQL
- Source Server Version : 50720
+ Source Server Version : 50717
  Source Host           : localhost:3306
  Source Schema         : task11
 
  Target Server Type    : MySQL
- Target Server Version : 50720
+ Target Server Version : 50717
  File Encoding         : 65001
 
- Date: 04/03/2018 23:18:20
+ Date: 05/03/2018 16:24:52
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for classlist
+-- ----------------------------
+DROP TABLE IF EXISTS `classlist`;
+CREATE TABLE `classlist`  (
+  `ID` int(20) NOT NULL AUTO_INCREMENT,
+  `classID` int(20) NOT NULL,
+  `className` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of classlist
+-- ----------------------------
+INSERT INTO `classlist` VALUES (0, 0, '闲散学生');
+INSERT INTO `classlist` VALUES (1, 1, '一班');
+INSERT INTO `classlist` VALUES (2, 2, '二班');
+INSERT INTO `classlist` VALUES (3, 3, '三班');
+INSERT INTO `classlist` VALUES (4, 4, '四班');
 
 -- ----------------------------
 -- Table structure for student
@@ -26,20 +46,16 @@ CREATE TABLE `student`  (
   `NAME` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `BIRTHDATE` varchar(10) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `ADDRESS` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
-  `CLASSID` int(10) NOT NULL,
+  `CLASSID` int(10) NOT NULL DEFAULT 0,
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 30 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES (1, '小花', '19941209', '成都', 0);
-INSERT INTO `student` VALUES (2, '小明', '19940506', '上海', 1);
-INSERT INTO `student` VALUES (3, '小猪', '19940506', '北京', 1);
-INSERT INTO `student` VALUES (4, '小飞', '17780306', '西贡', 0);
-INSERT INTO `student` VALUES (5, '咲爱', '18891205', '名典', 3);
-INSERT INTO `student` VALUES (6, '晓明', '18251603', '西欧', 3);
-INSERT INTO `student` VALUES (7, '小明', '15890603', '豆腐汤', 2);
+INSERT INTO `student` VALUES (16, '权限管理', '19941212', '东京', 0);
+INSERT INTO `student` VALUES (17, '产品上架', '19941212', '东京', 0);
+INSERT INTO `student` VALUES (18, '后端开发', '19941212', '东京', 0);
 
 -- ----------------------------
 -- Table structure for usersinfo
